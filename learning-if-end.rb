916913908c923833729bf8c0268488059b8362f5
Stefani Waddell <stefani.waddell@hotmail.com>
2020-03-20 04:55:38 +0000

    Automatically backed up by Learn

---
+++ learning-if-end.rb
@@ -0,0 +1,32 @@
+#run_code_inside = false
+#puts "Code before if...end"
+#if run_code_inside
+#  puts "code inside"
+#end
+#puts "Code after if...end"
+
+#chance_of_rain = 0.2 
+#puts "Let's go outside!"
+#if chance_of_rain > 0.5
+#  puts "Pack an umbrella!"
+#else
+#  puts "Enjoy the fine day!"
+#end
+#puts "Oh, and always wear sunscreen!"
+
+#current_weather = "raining"
+
+#case current_weather
+#  when "sunny"
+#    puts "grab some sunscreen!"
+#  when "raining"
+#    puts "grab an umbrella!"
+#  when "snowing"
+#    puts "bundle up!"
+#end
+
+counter = 0 
+until counter == 20
+  puts "The current number is #{counter} and is less than 20."
+  counter += 1 
+end
